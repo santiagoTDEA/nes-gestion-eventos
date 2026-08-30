25c5eb91a672848f2c214bfd416c2b46acc39512
santiagoTDEA <santiago.ospina11@correo.tdea.edu.co>
2026-08-30 15:58:09 -0500

    buid-30/08 (#7)
    * feat : workflow build (#1)
    
    * Feature/habilitar variables ev (#4)
    
    * feat : workflow build
    
    * feat: se habilita la configuración para utilizar las variables de entonor
    
    * feat: se instala y se configura class-validators para DTO (#6)

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
@@ -6,6 +8,14 @@ import { setupSwagger } from './infrastructure/docs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 
   setupSwagger(app);
 
