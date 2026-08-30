84a95b42a298e667f2b14e0b3ed870d90cfeb7db
Santiago Ospina <santiago.ospina11@correo.tdea.edu.co>
2026-08-30 15:37:56 -0500

    feat: se habilita la configuración para utilizar las variables de entonor

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { GetHelloUseCase } from './application/use-cases/get-hello.use-case';
 import { AppController } from './infrastructure/http/controllers/app.controller';
 import { RequestHeadersInterceptor } from './infrastructure/http/interceptors/request-headers.interceptor';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
   providers: [
     GetHelloUseCase,
