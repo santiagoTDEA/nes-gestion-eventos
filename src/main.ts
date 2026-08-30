5577c780466bb63b5ee207c9f2b9537943c97752
santiagoTDEA <santiago.ospina11@correo.tdea.edu.co>
2026-08-30 15:39:42 -0500

    Feature/habilitar variables ev (#3)
    * feat : workflow build
    
    * feat: se habilita la configuración para utilizar las variables de entonor

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 import { setupSwagger } from './infrastructure/docs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
 
   setupSwagger(app);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(Number(configService.get<string>('PORT', '3000')));
 }
 void bootstrap();
