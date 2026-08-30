c64cdecd8565c9821e42e205b81026d76289c2bc
Santiago Ospina <santiago.ospina11@correo.tdea.edu.co>
2026-08-30 15:55:10 -0500

    feat: se instala y se configura class-validators para DTO

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
-import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './infrastructure/docs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 
   setupSwagger(app);
 
